cdae2df217fd0d31ff8d8f6effe8e4867a17bfe0
graykode <nlkey2022@gmail.com>
2023-11-08 13:59:05 +0900

    feat: update contract address

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -22,9 +22,9 @@ export const CONTRACT_ADDRESSES: {
       '0x0000000000000000000000000000000000000000' as `0x${string}`,
   },
   [7777]: {
-    CouponOracle: '0x8831c769874fF23ED5DF0daacfD84Cc147335506' as `0x${string}`,
+    CouponOracle: '0xbaF0FF012884917b1FCb2222d6e6D75Eb795D23B' as `0x${string}`,
     LoanPositionManager:
-      '0xE0dBCB42CCAc63C949cE3EF879A647DDb662916d' as `0x${string}`,
+      '0xa87224d1F96cA183CE119f94b6e48035c93B05Fb' as `0x${string}`,
     LoanPositionLiquidateHelper:
       '0x0000000000000000000000000000000000000001' as `0x${string}`,
   },
