7ac233142072c2e774dbe93fbf950bbf727d04c2
Xavier <ladig1017@gmail.com>
2023-11-10 14:23:15 +0900

    refactor: remove helper address

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -5,14 +5,11 @@ const CHAIN_ID: ChainID = Number(process.env.CHAIN_ID) as ChainID
 export const CONTRACT_ADDRESSES: {
   CouponOracle: `0x${string}`
   LoanPositionManager: `0x${string}`
-  LoanPositionLiquidateHelper: `0x${string}`
   CouponLiquidator: `0x${string}`
 } = {
   [42161]: {
     CouponOracle: '0x0000000000000000000000000000000000000000' as `0x${string}`,
     LoanPositionManager:
-      '0x0000000000000000000000000000000000000000' as `0x${string}`,
-    LoanPositionLiquidateHelper:
       '0x0000000000000000000000000000000000000000' as `0x${string}`,
     CouponLiquidator:
       '0x0000000000000000000000000000000000000000' as `0x${string}`,
@@ -21,8 +18,6 @@ export const CONTRACT_ADDRESSES: {
     CouponOracle: '0x0000000000000000000000000000000000000000' as `0x${string}`,
     LoanPositionManager:
       '0x0000000000000000000000000000000000000000' as `0x${string}`,
-    LoanPositionLiquidateHelper:
-      '0x0000000000000000000000000000000000000000' as `0x${string}`,
     CouponLiquidator:
       '0x0000000000000000000000000000000000000000' as `0x${string}`,
   },
@@ -30,8 +25,6 @@ export const CONTRACT_ADDRESSES: {
     CouponOracle: '0xbaF0FF012884917b1FCb2222d6e6D75Eb795D23B' as `0x${string}`,
     LoanPositionManager:
       '0xa87224d1F96cA183CE119f94b6e48035c93B05Fb' as `0x${string}`,
-    LoanPositionLiquidateHelper:
-      '0x0000000000000000000000000000000000000001' as `0x${string}`,
     CouponLiquidator:
       '0x8EAEDF34bd1fcbb5A81CAD76FA2B3DFEF616a317' as `0x${string}`,
   },
